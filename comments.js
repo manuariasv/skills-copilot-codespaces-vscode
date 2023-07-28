2c62dc3a8dd765109c84b7f4e886bd8f299dfde2
manuariasv <140855298+manuariasv@users.noreply.github.com>
2023-07-28 22:24:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,6 +1,23 @@
 // Create web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { randomBytes } = require('crypto');
-const axios = require('axios');
+// http://localhost:9000/comments
+// http://localhost:9000/comments/1
+// http://localhost:9000/comments/2
+
+// http://localhost:9000/comments?postId=1
+// http://localhost:9000/comments?postId=2
+// http://localhost:9000/comments?postId=3
+
+// http://localhost:9000/comments?postId=1&userId=1
+// http://localhost:9000/comments?postId=1&userId=2
+// http://localhost:9000/comments?postId=1&userId=3
+
+// http://localhost:9000/comments?userId=1
+// http://localhost:9000/comments?userId=2
+// http://localhost:9000/comments?userId=3
+
+// http://localhost:9000/comments?userId=1&postId=1
+// http://localhost:9000/comments?userId=2&postId=2
+// http://localhost:9000/comments?userId=3&postId=3
+
+// http://localhost:9000/comments?userId=1&postId=1&email=Eliseo@gardner
+// http://localhost:
